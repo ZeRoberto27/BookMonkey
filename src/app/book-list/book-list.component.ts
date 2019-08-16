d3528e1e0e276374dc7712204fa5daf9f52da0f8
ZeRoberto27 <robert.schlager@gmail.com>
2019-08-16 16:58:28 +0200

    navigation with routing

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
 
@@ -9,16 +9,11 @@ import { BookStoreService } from '../shared/book-store.service';
 })
 export class BookListComponent implements OnInit {
   books: Book[];
-  @Output() showDetailsEvent = new EventEmitter<Book>();
 
   constructor(private bs: BookStoreService) { }
 
   ngOnInit() {
     this.books = this.bs.getAll();
-  }
-
-  showDetails(book: Book) {
-    this.showDetailsEvent.emit(book);
   }
 
 }
