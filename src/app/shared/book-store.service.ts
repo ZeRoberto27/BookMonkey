9e1080ca99b2affb92280e78bd98ae9d06cb30bd
ZeRoberto27 <robert.schlager@gmail.com>
2019-08-20 14:41:43 +0200

    Typehead Suche mit Observables

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -20,6 +20,16 @@ export class BookStoreService {
     return throwError(error);
   }
 
+  getAllSearch(searchTerm: string): Observable<Book[]> {
+    return this.http.get<BookRaw[]>(
+      `${this.api}/books/search/${searchTerm}`
+    ).pipe(
+      retry(3),
+      map(booksRaw => booksRaw.map(b => BookFactory.fromRaw(b))),
+      catchError(this.errorHandler)
+    );
+  }
+
   getAll(): Observable<Book[]> {
     return this.http.get<BookRaw[]>(
       `${this.api}/books`
