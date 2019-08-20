801b78c7705e40d58107317c3149937b68663865
ZeRoberto27 <robert.schlager@gmail.com>
2019-08-20 14:04:40 +0200

    Observer und ErrorHandling

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Book } from './book';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, retry, catchError } from 'rxjs/operators';
+import { BookRaw } from './book-raw';
+import { BookFactory } from './book-factory';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +15,29 @@ export class BookStoreService {
   constructor(private http: HttpClient) {
   }
 
+  private errorHandler(error: HttpErrorResponse): Observable<any> {
+    console.error('Fehler aufgetreten!');
+    return throwError(error);
+  }
+
   getAll(): Observable<Book[]> {
-    return this.http.get<any[]>(`${this.api}/books`);
+    return this.http.get<BookRaw[]>(
+      `${this.api}/books`
+    ).pipe(
+      retry(3),
+      map(booksRaw => booksRaw.map(b => BookFactory.fromRaw(b))),
+      catchError(this.errorHandler)
+    );
   }
 
   getSingle(isbn: string): Observable<Book> {
-    return this.http.get<any>(`${this.api}/book/${isbn}`);
+    return this.http.get<BookRaw>(
+      `${this.api}/book/${isbn}`
+    ).pipe(
+      retry(3),
+      map(b => BookFactory.fromRaw(b)),
+      catchError(this.errorHandler)
+    );
   }
 
   remove(isbn: string): Observable<any> {
