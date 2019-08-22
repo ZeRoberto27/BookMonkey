ce19235f8f792e447b65a0499bebbbf78fc107cc
ZeRoberto27 <robert.schlager@gmail.com>
2019-08-22 15:23:41 +0200

    Buch bearbeiten mit Reactive Forms

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -1,28 +1,104 @@
-import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
-import { BookFactory } from '../shared/book-factory';
+import { Component, OnInit, Output, EventEmitter, Input, OnChanges } from '@angular/core';
 import { Book } from '../shared/book';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+import { Thumbnail } from '../shared/thumbnail';
 
 @Component({
   selector: 'bm-book-form',
   templateUrl: './book-form.component.html',
   styleUrls: ['./book-form.component.css']
 })
-export class BookFormComponent implements OnInit {
-  book = BookFactory.empty();
-
+export class BookFormComponent implements OnInit, OnChanges {
+  bookForm: FormGroup;
   @Output() submitBook = new EventEmitter<Book>();
-  @ViewChild('bookForm', {static: false}) bookForm: NgForm;
+  @Input() book: Book;
+  @Input() editing = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  ngOnChanges(): void {
+    this.initForm();
+    this.setFormValues(this.book);
+  }
+
+  private initForm() {
+    if (this.bookForm) { return; }
+
+    this.bookForm = this.fb.group({
+      title: ['', Validators.required],
+      subtitle: [''],
+      isbn: [{ value: '', disabled: this.editing }, [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13)
+      ]],
+      description: [''],
+      authors: this.buildAuthorsArray(['']),
+      thumbnails: this.buildThumbnailsArray([
+        { title: '', url: ''}
+      ]),
+      published: []
+    });
+  }
+
+  private setFormValues(book: Book) {
+    this.bookForm.patchValue(book);
+
+    this.bookForm.setControl(
+      'authors',
+      this.buildAuthorsArray(book.authors)
+    );
+
+    this.bookForm.setControl(
+      'thumbnails',
+      this.buildThumbnailsArray(book.thumbnails)
+    );
+  }
+
+  private buildAuthorsArray(values: string[]): FormArray {
+    return this.fb.array(values, Validators.required);
+  }
+
+  private buildThumbnailsArray(values: Thumbnail[]): FormArray {
+    return this.fb.array(values.map(t => this.fb.group(t)));
+  }
+
+  get authors(): FormArray {
+    return this.bookForm.get('authors') as FormArray;
+  }
+
+  get thumbnails(): FormArray {
+    return this.bookForm.get('thumbnails') as FormArray;
+  }
+
+  addAuthorControl() {
+    this.authors.push(this.fb.control(''));
+  }
+
+  addThumbnailControl() {
+    this.thumbnails.push(this.fb.group({ url: '', title: '' }));
   }
 
   submitForm() {
-    this.submitBook.emit(this.book);
+    const formValue = this.bookForm.value;
 
-    this.book = BookFactory.empty();
+    const authors = formValue.authors.filter(author => author);
+    const thumbnails = formValue.thumbnails.filter(thumbnail => thumbnail.url);
+
+    const isbn = this.editing ? this.book.isbn : formValue.isbn;
+
+    const newBook: Book = {
+      ...formValue,
+      isbn,
+      authors,
+      thumbnails
+    };
+
+    this.submitBook.emit(newBook);
     this.bookForm.reset();
   }
 
