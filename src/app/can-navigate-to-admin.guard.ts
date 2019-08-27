8f92dc9066e45f8dbd28be34e9135dab9c8f4565
ZeRoberto27 <robert.schlager@gmail.com>
2019-08-27 11:17:15 +0200

    guards

---
+++ src/app/can-navigate-to-admin.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CanNavigateToAdminGuard implements CanActivate {
+  accessGranted = false;
+
+  canActivate(): boolean {
+    if (!this.accessGranted) {
+      this.accessGranted = window.confirm('Mit großer Macht kommt große Verantwortung. Möchten Sie den Admin-Bereich betreten?');
+    }
+
+    return this.accessGranted;
+  }
+}
